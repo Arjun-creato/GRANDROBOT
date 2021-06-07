4e6fe49fa41fc7cb1d43dc1bbba25b950884c4b8
Arjun-creato <73528330+Arjun-creato@users.noreply.github.com>
2021-06-07 13:26:54 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -27,7 +27,7 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
 **Hello {}, My Name is {}!** 
-I am an **SUPERB**  group management bot.
+I am an  group management bot.
 You can find the list of available commands with /help.
 
 """
@@ -142,11 +142,11 @@ def start(bot: Bot, update: Update, args: List[str]):
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🤝HELP🤝",
                                                                        callback_data="help_back".format(bot.username)),
                                                                                    InlineKeyboardButton(text="🧑‍💻My Creator🧑‍💻",
-                                                                       url="t.me/Pabloescobar_colombia")],
+                                                                       url="Elchapo")],
                                                                                    [InlineKeyboardButton(text="ADD THIS BOT TO YOUR GROUP",
                                                                        url="t.me/{}?startgroup=true".format(bot.username)),
-                                                                                   InlineKeyboardButton(text="Source Code",
-                                                                       url="https://github.com/legendx22/GRANDROBOT")
+                                                                                   InlineKeyboardButton(text="its420somewhere",
+                                                                       url="https://t.me/its420somewhere")
                                                                                  ]]))
 
 
